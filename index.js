58f4d44c8e9cfee8bf36b5238baf1259210cb1a7
Marcel Ghostine <marcel.ghostine@releye.se>
2024-04-18 15:40:48 +0200

    new logic for app.get /webhook

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-/* const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const app = express();
-
-app.use('/', createProxyMiddleware({
-    target: 'https://your-salesforce-instance.com/services/authcallback/strava',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/': '/', // rewrite path
-    },
-    onProxyRes: function (proxyRes, req, res) {
-        proxyRes.headers['access-control-allow-origin'] = '*';
-    }
-}));
-
-app.listen(3000); */
 const express = require('express');
 const app = express();
 app.use(express.json());
@@ -25,8 +8,21 @@ app.post('/webhook', (req, res) => {
 });
 
 app.get('/webhook', (req, res) => {
-    const challenge = req.query['hub.challenge'];
-    res.json({ 'hub.challenge': challenge });
+    const VERIFY_TOKEN = "your_verify_token";
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.json({ "hub.challenge": challenge });
+        } else {
+            res.sendStatus(403);
+        }
+    } else {
+        res.sendStatus(400);
+    }
 });
 
 app.get('/authcallback', (req, res) => {
@@ -35,10 +31,8 @@ app.get('/authcallback', (req, res) => {
 
     res.redirect(redirectUri);
 });
+
 app.get('/test', (req, res) => {
     console.log('Test route hit');
     res.send('Test route hit');
 });
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server listening on port ${port}`));
