4fb3f2e5274fa68334f49c38fbd2d6d84943d69e
Marcel Ghostine <marcel.ghostine@releye.se>
2024-04-19 09:14:32 +0200

    removed /webhook - /

--- index.js
+++ index.js
@@ -1,25 +1,8 @@
-/* const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-const app = express();
-
-app.use('/', createProxyMiddleware({
-    target: 'https://your-salesforce-instance.com/services/authcallback/strava',
-    changeOrigin: true,
-    pathRewrite: {
-        '^/': '/', // rewrite path
-    },
-    onProxyRes: function (proxyRes, req, res) {
-        proxyRes.headers['access-control-allow-origin'] = '*';
-    }
-}));
-
-app.listen(3000); */
 const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.post('/webhook', (req, res) => {
+app.post('/', (req, res) => {
     console.log('Received a webhook:', req.body);
 
     // The object containing the event details is in req.body
